ed3765d20c1a670648cce25c9eec89513aca1e76
gregory.verghese@gmail.com <greg.verghese@gmail.com>
2021-07-07 15:42:14 +0100

    added more parameters to name based on values in config

--- src/tuning.py
+++ src/tuning.py
@@ -21,6 +21,7 @@ from main import main
 __author__ = 'Gregory Verghese'
 __email__ = 'gregory.verghese@kcl.ac.uk'
 
+N=20
 
 def tuning(args):
 
@@ -32,7 +33,7 @@ def tuning(args):
     Args:
         command line arguments for script
     '''
-
+    
     date = str(datetime.date.today())
     currentTime = datetime.datetime.now().strftime('%H:%M')
     resultsPath = os.path.join(args['outpath'], 'summaries')
@@ -50,10 +51,9 @@ def tuning(args):
     losses = jsonDict['loss']
     #dropouts = jsonDict['dropouts']
     augmentation = jsonDict['augmentation']['methods'] 
-
+    augmentation=augmentation*N
     feature = jsonDict['feature']
     mag = jsonDict['magnification']
-
     #Loop over parameters (augmentation and loss functions)
     for a in augmentation:
        for l in losses:
@@ -69,7 +69,14 @@ def tuning(args):
           #name = name.replace('$drop', str(d))
           augInitials = [i[0] for i in a]
           name = name.replace('$augment', ''.join(augInitials))
-
+          name=name.replace('$optimizer',jsonDict['optimizer']['method'])
+          name=name.replace('$epoch',str(jsonDict['epoch']))
+          name=name.replace('$dim',str(jsonDict['imageDims']))
+          
+          name=name.replace('$uptypename',str(jsonDict['upTypeName']))
+          name=name.replace('$threshold',str(jsonDict['activationthreshold']))
+          name=name.replace('$feature',str(jsonDict['feature']))
+          name=name.replace('$magnification',str(jsonDict['magnification']))
           jsonDict['modelname'] = name
           
           #save down analysis specific config file
