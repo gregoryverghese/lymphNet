d596877c343d8ac6b12298757ecf346eb2e6b694
gregory.verghese@gmail.com <greg.verghese@gmail.com>
2021-07-07 15:43:00 +0100

    updated bash file with correct directories

--- bash/dl_segmentation.sh
+++ bash/dl_segmentation.sh
@@ -6,11 +6,11 @@
 #$ -R y
 #$ -l h_rt=16:00:00
 #$ -j y
-#$ -N 'unet_g_10x_gaussian_noise'
+#$ -N 'unet_g_2.5x'
 
 source /share/apps/source_files/python/python-3.7.0.source
 source /share/apps/source_files/cuda/cuda-10.1.source
 export LD_LIBRARY_PATH=/share/apps/TensorRT-5.1.5.0/lib:$LD_LIBRARY_PATH
-python3 /home/verghese/breastcancer_ln_deeplearning/scripts/python/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/data/patches/segmentation/10x/one/data_4/tfrecords -rd tfrecords_g_256_32 -cf /home/verghese/breastcancer_ln_deeplearning/scripts/config/config_germinal_template.json -cp checkpoints -op /home/verghese/breastcancer_ln_deeplearning/output/ -mn unet
 
-
+python3 /home/verghese/lymphnode-keras/lymphnode-keras/src/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/data/patches/segmentation/10x/one/data_4/tfrecords -rd tfrecords_g_256_32 -cf /home/verghese/lymphnode-keras/lymphnode-keras/config/config_germinal_template.json -cp checkpoints -op /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/output -mn unet
+n3 /home/verghese/lymphnode-keras/lymphnode-keras/src/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode/data/patches/segmentation/2.5x/one/data_4/tfrecords -rd tfrecords_g_256_32 -cf /home/verghese/lymphnode-keras/lymphnode-keras/config/config_germinal_template.json -cp checkpoints -op /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/output -mn unet
