6fb1bf427d1cc0d025e3fab5c1f27e453b599d82
gregory.verghese@gmail.com <greg.verghese@gmail.com>
2022-05-02 20:26:07 +0100

    added in dice coefficient

--- src-2/predict-2.py
+++ src-2/predict-2.py
@@ -11,8 +11,9 @@ from tensorflow.keras.models import load_model
 from utilities.evaluation import diceCoef
 
 
-image_path='/SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/segmentation/10x/one/testing/images/germinal'
+test_path='/SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/segmentation/10x/one/testing'
 model_path='/SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/output/models/2022-04-28/attention_sinus_2.5x_adam_weightedBinaryCrossEntropy_FRC_data4_256_01:19.h5'
+save_path='/home/verghese/lymphnode-keras'
 
 
 class Predict():
@@ -30,46 +31,54 @@ class Predict():
 
     def _predict(self, image):
         y_dim, x_dim, _ = image.shape
-        canvas=np.zeros((int(y_dim),int(x_dim),3))
-        print(canvas.shape)
+        canvas=np.zeros((1,int(y_dim),int(x_dim),1))
         for x, y in self._patching(x_dim,y_dim):
-            patch=image[x:x+self.step,y:y+self.step,:]
-            #patch=np.expand_dims(patch,axis=0)
-            #probs=self.model.predict(patch)
-            #prediction=tf.cast((probs>self.threshold), tf.float32)
-            #canvas[x:x+self.step,y:y+self.step]=prediction[0,:,:,0] 
-            canvas[x:x+self.step,y:y+self.step,:]=patch
-
+            patch=image[y:y+self.step,x:x+self.step,:]
+            patch=np.expand_dims(patch,axis=0)
+            probs=self.model.predict(patch)
+            prediction=tf.cast((probs>self.threshold), tf.float32)
+            canvas[:,y:y+self.step,x:x+self.step,:]=prediction
         return canvas.astype(np.uint8)
 
 
-def test_predictions(model,image_path,threshold,step):
-    
-    image_paths=glob.glob(os.path.join(image_path,'*'))
+def test_predictions(model,test_path,save_path,threshold=0.5,step=512):
+    dices=[]
+    names=[]
+    image_paths=glob.glob(os.path.join(test_path,'images/germinal','*'))
+    mask_paths=glob.glob(os.path.join(test_path,'masks/germinal','*'))
     predict=Predict(model,threshold,step)
-    for i, path in enumerate(image_paths):
-        print(os.path.basename(path))
-        image=cv2.imread(path) 
+    for i, (i_path,m_path) in enumerate(zip(image_paths,mask_paths)):
+        name=os.path.basename(i_path)
+        names.append(name)
+        mask=cv2.imread(m_path)
+        image=cv2.imread(i_path) 
         prediction=predict._predict(image)
-        cv2.imwrite(os.path.join('/home/verghese/lymphnode-keras',str(i))+'.png',prediction)
-    print('final prediction', prediction.shape)
+        mask=np.expand_dims(mask,axis=0)
+        dices.append(diceCoef(prediction,mask))
+        cv2.imwrite(os.path.join(save_path,'predictions',name+'.png'),prediction*255)
+        print(names[i],dices[i])
+    dice_df=pd.DataFrame({'names':names,'dices':dices})
+    dice_df.to_csv(os.path.join(save_path,'results.csv'))
 
 model=load_model(model_path)
-test_predictions(model,image_path,0.5,1024)
+test_predictions(model,test_path,save_path,0.5,512)
 
-
-"""  
+  
 if __name__=='__main__':
     ap=argparse.ArgumentParser(description='model inference')
     ap.add_argument('-mp','--model_path',required=True,help='path to trained model')
-    ap.add_argument('-ip','--image_path',required=True,help='path to test images')
+    ap.add_argument('-ip','--test_path',required=True,help='path to test images and masks')
+    ap.add_argument('-ip','--save_path',required=True,help='experiment folder for saving results')
     ap.add_argument('-th','--threshold',default=0.5,help='activation threshold')
     ap.add_argument('-s','--step',default=512,help='sliding window size')
     args=ap.parse_args()
 
     model=load_model(args.model_path)
-    test_predictions(model,args.image_path,args.threshold,args.step)
-"""
+    test_predictions(model,
+                     args.test_path,
+                     args.save_path,
+                     args.threshold,
+                     args.step)
     
 
 
