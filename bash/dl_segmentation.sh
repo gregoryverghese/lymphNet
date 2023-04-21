aaaefc476c061fc0be7c62991f3dc9b61e8a9202
Holly Rafique <holly.rafique@kcl.ac.uk>
2023-04-21 09:40:36 +0100

    1) update bash file to work on Holly's env \n2) update config for first run on new branch/env

--- bash/dl_segmentation.sh
+++ bash/dl_segmentation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#$ -l tmem=6G
+#$ -l tmem=10G
 #$ -l gpu=true
-#$ -pe gpu 4
+#$ -pe gpu 2
 #$ -R y
-#$ -l h_rt=16:00:00
+#$ -l h_rt=24:00:00
 #$ -j y
-#$ -N 'attention_g_2.5x'
+#$ -N 'multiscale_defaults'
 
 source /share/apps/source_files/python/python-3.7.0.source
-source /share/apps/source_files/cuda/cuda-10.1.source
-export LD_LIBRARY_PATH=/share/apps/TensorRT-5.1.5.0/lib:$LD_LIBRARY_PATH
-python3 /home/verghese/lymphnode-keras/lymphnode/src/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/segmentation/2.5x/one/data_4/tfrecords -rd tfrecords_g_256_32 -cf /home/verghese/lymphnode-keras/lymphnode/config/config_germinal_template.json -cp checkpoints -op /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/output -mn attention
+source /share/apps/source_files/cuda/cuda-11.2.source
+export LD_LIBRARY_PATH=/share/apps/TensorRT-6.0.1.8/lib:$LD_LIBRARY_PATH
+python3 /home/hrafique/lymphnode-keras/lymphnode/src/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/v1/10x/tfrecords_2 -rd tfrecords_g_1024_256 -cf /home/hrafique/lymphnode-keras/lymphnode/config/config_germinal.yaml -cp checkpoints -op /SAN/colcc/WSI_LymphNodes_BreastCancer/HollyR/output -tp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/v1/10x/testing -p True -mn multiscale
