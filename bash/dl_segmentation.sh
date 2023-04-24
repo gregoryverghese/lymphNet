e834cd6082372a9d0e8ec01f143ec68bc9f32476
Holly Rafique <holly.rafique@kcl.ac.uk>
2023-04-24 09:27:54 +0100

    updated file paths for running multiscale

--- bash/dl_segmentation.sh
+++ bash/dl_segmentation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#$ -l tmem=10G
+#$ -l tmem=100G
 #$ -l gpu=true
 #$ -pe gpu 2
 #$ -R y
@@ -11,4 +11,4 @@
 source /share/apps/source_files/python/python-3.7.0.source
 source /share/apps/source_files/cuda/cuda-11.2.source
 export LD_LIBRARY_PATH=/share/apps/TensorRT-6.0.1.8/lib:$LD_LIBRARY_PATH
-python3 /home/hrafique/lymphnode-keras/lymphnode/src/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/v1/10x/tfrecords_2 -rd tfrecords_g_1024_256 -cf /home/hrafique/lymphnode-keras/lymphnode/config/config_germinal.yaml -cp checkpoints -op /SAN/colcc/WSI_LymphNodes_BreastCancer/HollyR/output -tp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/v1/10x/testing -p True -mn multiscale
+python3 /home/hrafique/lymphnode-keras/src/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/v1/10x/tfrecords_2 -rd tfrecords_g_1024_256 -cf /home/hrafique/lymphnode-keras/config/config_germinal.yaml -cp checkpoints -op /SAN/colcc/WSI_LymphNodes_BreastCancer/HollyR/output -tp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/v1/10x/testing -p True -mn multiscale
