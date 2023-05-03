1cdccfd319f1988307888b6ae1ec04fe7176f208
Mengyuan-Li1996 <mengyuan.li96@outlook.com>
2023-05-03 11:07:16 +0100

    updated_loaded packages

--- bash/dl_segmentation.sh
+++ bash/dl_segmentation.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-#$ -l tmem=10G
+#$ -l tmem=6G
 #$ -l gpu=true
-#$ -pe gpu 2
+#$ -pe gpu 4
 #$ -R y
 #$ -l h_rt=16:00:00
 #$ -j y
-#$ -N 'attn-gc-v1-10x'
-hostname
-date
+#$ -N 'attention_g_2.5x'
+
 source /share/apps/source_files/python/python-3.7.0.source
-source /share/apps/source_files/cuda/cuda-11.2.source
+source /share/apps/source_files/cuda/cuda-10.1.source
+#export LD_LIBRARY_PATH=/share/apps/TensorRT-5.1.5.0/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/share/apps/TensorRT-6.0.1.8/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
-python3 /home/hrafique/lymphnode-keras/src/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/v1/10x/tfrecords_2 -rd tfrecords_g_1024_256 -cf /home/hrafique/lymphnode-keras/config/config_germinal.yaml -cp checkpoints -op /SAN/colcc/WSI_LymphNodes_BreastCancer/HollyR/output -tp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/v1/10x/testing -p True -mn attention
-date
+python3  /home/mengyuli/projects/BreastCancer/script_mod_Mengyuan/20230424_Holly/lymphnode-keras/src/tuning.py -rp /SAN/colcc/WSI_LymphNodes_BreastCancer/Greg/lymphnode-keras/data/patches/segmentation/2.5x/one/data_4/tfrecords -rd tfrecords_g_256_32 -cf /home/mengyuli/projects/BreastCancer/script_mod_Mengyuan/script_of_Mengyuan/config/config_germinal_template_2.json -cp checkpoints -op /SAN/colcc/WSI_LymphNodes_BreastCancer/Mengyuan/try -mn attention -tp /SAN/colcc/WSI_LymphNodes_BreastCancer/HollyR/data/patches/testing/baseline -p True
