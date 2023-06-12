c5b9405f5e282c91d32c76ef5ce3e5e7650e0eb9
Holly Rafique <holly.rafique@kcl.ac.uk>
2023-06-12 11:30:55 +0100

    specify target filename, remove LuminosityStandardize as it led to negative OD matrix valued

--- src/preprocessing/stain_normalisation.py
+++ src/preprocessing/stain_normalisation.py
@@ -24,29 +24,33 @@ GAN_METHODS = ['gan', 'staingan']
 NET_METHODS = ['net', 'stainnet'] 
 
 
-def stain_norm(image_path, target_path, output_path, method_name):
+def stain_norm(image_path, target_path, target_name, output_path, method_name):
 
 
     
     #can use staintools module for vahadane and macenko
     if(method_name.lower() in STAINTOOLS_METHODS):
-        staintools_norm(image_path, target_path, output_path, method_name) 
+        staintools_norm(image_path, target_path, target_name, output_path, method_name) 
   
     #elif(method_name.lower() in GAN_METHODS):
     #elif(method_name.lower() in NET_METHODS):
 
-def staintools_norm(image_path, target_path, output_path, method_name):
+def staintools_norm(image_path, target_path, target_name, output_path, method_name):
  
     #read all images names in directories
-    target_paths=glob.glob(os.path.join(target_path,'*'))
+    #target_paths=glob.glob(os.path.join(target_path,'*.png'))
+    target_path=os.path.join(target_path,target_name)
     image_paths=glob.glob(os.path.join(image_path,'*'))
     print(image_paths)
     #define ideal target patch and train normaliser
     #if there is more than one image in the target folder we will only read the first
-    target = staintools.read_image(target_paths[0])
-    target = staintools.LuminosityStandardizer.standardize(target)
+    target = staintools.read_image(target_path)
+    #target = staintools.LuminosityStandardizer.standardize(target)
     print("target defined")
     normalizer = staintools.StainNormalizer(method=method_name)
+
+    print("Stain Matrix:",normalizer.extractor.get_stain_matrix(target))
+    
     normalizer.fit(target)
     print("fitted")
     #during testing we will just try a few images at once
@@ -99,6 +103,7 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-ip', '--input_path', required=True, help='path to images to be normalised')
     ap.add_argument('-tp', '--target_path', help='path to target image - should only be one image in the directory')
+    ap.add_argument('-tn', '--target_name', default='staintarget.png', help='name of target image')
     ap.add_argument('-op', '--output_path', required=True, help='path for saving normalised images')
     ap.add_argument('-mn', '--method_name', required=True, help='name of normalisation method: vahadane/macenko/GAN') 
 
@@ -117,4 +122,4 @@ if __name__ == '__main__':
     os.makedirs(output_path,exist_ok=True)
 
 
-    stain_norm(args.input_path, target_path, output_path, args.method_name)
+    stain_norm(args.input_path, target_path, args.target_name, output_path, args.method_name)
