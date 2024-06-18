corrected tf dtypes to use mixed precision policy properly

src/data/tfrecord_read.py
@@ -171,8 +171,8 @@ class TFRecordLoader():
         dataset = dataset.with_options(ignoreDataOrder)
         dataset = dataset.interleave(lambda x: tf.data.TFRecordDataset(x), cycle_length=16, num_parallel_calls=AUTO)
         dataset = dataset.map(self._read_tfr_record, num_parallel_calls=AUTO)
-        #f1=tf.cast(tf.reshape(x,(self.tile_dims,self.tile_dims,3)),tf.float16)
-        dataset= dataset.map(lambda x, y: (tf.cast(tf.reshape(x,(self.tile_dims,self.tile_dims,3)),tf.float16),tf.cast(tf.reshape(y,(self.tile_dims,self.tile_dims,3)),tf.float16)))
+        #f1=tf.cast(tf.reshape(x,(self.tile_dims,self.tile_dims,3)),tf.float32)
+        dataset= dataset.map(lambda x, y: (tf.cast(tf.reshape(x,(self.tile_dims,self.tile_dims,3)),tf.float32),tf.cast(tf.reshape(y,(self.tile_dims,self.tile_dims,3)),tf.float32)))
         #dataset = dataset.map(f2)
         dataset = dataset.map(lambda x, y: (x, y[:,:,0:1]), num_parallel_calls=AUTO) #changed from 4 to AUTO
         if self.task_type=='multi':

src/distributed_train.py
@@ -31,15 +31,11 @@ __email__ = 'gregory.verghese@kcl.ac.uk'
 #tf.__dict__["gradients"] = memory_saving_gradients.gradients_speed
 
 DEBUG = True
-#PROFILE = False
 
 ##added by HR 02/06/2024
 # Enable mixed precision training
-# setting in main.py
+#sest in main.py
 #mixed_precision.set_global_policy('mixed_float16')
-
-#os.environ["WANDB_MODE"] = "offline"
-
 
 def get_gpu_memory_used():
     command = "nvidia-smi --query-gpu=memory.used --format=csv,nounits,noheader"
@@ -81,10 +77,7 @@ class DistributedTraining():
         self.criterion = criterion
         self.strategy = strategy
         self.epochs = epoch
-        # HR 12/06/2024
-        # try adjusting this based on num replics 
         self.global_batch_size = global_batch_size
-        #self.global_batch_size = global_batch_size * strategy.num_replicas_in_sync
         self.metric = diceCoef
         self.img_dims = img_dims
 
@@ -104,20 +97,6 @@ class DistributedTraining():
         self.save_path = save_path
         self.config = config
         self.name = name
-
-    ### HR 12/06/2024 - overhauling compute_los and compute_dice in an attempt to make 4GPUs work
-    def NEWcompute_loss(self, label, predictions):
-        per_example_loss = self.criterion(label, predictions)
-        per_example_loss = tf.reshape(per_example_loss, [self.global_batch_size, -1])
-        #return tf.nn.compute_average_loss(per_example_loss, global_batch_size=self.global_batch_size)
-        return tf.reduce_sum(per_example_loss) / (1024*1024*self.global_batch_size)
-
-    def NEWcompute_dice(self, y_true, y_pred):
-        # Compute dice for each example in the batch
-        per_example_dice = tf.reduce_mean([self.metric(y_true[:,:,:,i], y_pred[:,:,:,i]) for i in range(y_true.shape[-1])], axis=0)
-        return per_example_dice
-
-
 
     def compute_loss(self, label, predictions):
         '''
@@ -196,7 +175,7 @@ class DistributedTraining():
         
         loss = self.criterion(y, logits)
         #print(loss)
-        y_pred = tf.cast((logits > self.threshold), tf.float32) ##tf.float32)
+        y_pred = tf.cast((logits > self.threshold), tf.float32) 
         #print(np.sum(y_pred))
         
         dice = self.compute_dice(y, y_pred)
@@ -207,8 +186,6 @@ class DistributedTraining():
         #loss = tf.reduce_sum(loss) * (1. / (self.img_dims*self.img_dims*1))
         #should we just call self.compute_loss like we do in _train_step
         #it contains an additional line to div by the num of replicas
-
-        ## HR 12/06/2024 - consider removing this line
         loss = tf.reduce_sum(loss) * (1. /(self.img_dims*self.img_dims*self.global_batch_size))
         return loss, dice
 
@@ -293,7 +270,7 @@ class DistributedTraining():
         return stop
 
 
-    def forward(self, run_profiling=False):
+    def forward(self,run_profiling=False):
         '''
         performs the forward pass of the network. calls each training epoch
         and prediction on validation data. Records results in history
@@ -333,6 +310,7 @@ class DistributedTraining():
             #trainLoss, trainDice = self.distributedTrainEpoch(trainDistDataset)
 
             ########## TRAIN THE MODEL ####################
+            #the _r keyword argument makes this trace event get processed as a step event by the Profileri
             if PROFILE:
                 #the _r keyword argument makes this trace event get processed as a step event by the Profiler
                 with tf.profiler.experimental.Trace('train', step_num=epoch, _r=1):
@@ -354,7 +332,7 @@ class DistributedTraining():
             tf.print(epoch_str.format(epoch+1, self.epochs, train_loss, train_dice, lr), end="")
 
 
-            ###### VALIDATION ###################i
+            ###### VALIDATION ###################
             if PROFILE:
                 with tf.profiler.experimental.Trace('validation', step_num=epoch, _r=1):
                     test_loss, test_dice  =  self._test()
@@ -445,7 +423,6 @@ class DistributedTraining():
             # Stop TensorFlow Profiler
             tf.profiler.experimental.stop(save=True)
             print("profiler stopped")
-
         if wandb_active:
             print("saving wandb")
             try:

src/main.py
@@ -32,7 +32,6 @@ from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.callbacks import LearningRateScheduler
 #from tensorflow.keras.utils import multi_gpu_model
 from tensorflow.keras import mixed_precision
-from tensorflow.python.client import device_lib  
 
 from distributed_train import DistributedTraining 
 from models import fcn8,unet,mobile,resunet,resunet_a,unet_mini,atten_unet
@@ -71,28 +70,7 @@ LOSSFUNCTIONS={
               }
 
 # Enable mixed precision
-# mixed_precision.set_global_policy('mixed_float16')
-def get_gpu_compute_capability():
-    local_device_protos = device_lib.list_local_devices()
-    for device in local_device_protos:
-        if device.device_type == 'GPU':
-            compute_capability = device.physical_device_desc.split('compute capability: ')[-1]
-            major, minor = compute_capability.split('.')
-            return int(major), int(minor)
-    return None, None
-
-def set_precision_policy():
-    major, minor = get_gpu_compute_capability()
-    if major is None:
-        print("No GPU found. Defaulting to float32.")
-        tf.keras.mixed_precision.set_global_policy('float32')
-    elif major >= 7:
-        print(f"GPU with compute capability {major}.{minor} detected. Setting mixed precision policy.")
-        tf.keras.mixed_precision.set_global_policy('mixed_float16')
-    else:
-        print(f"GPU with compute capability {major}.{minor} detected. Defaulting to float32.")
-        tf.keras.mixed_precision.set_global_policy('float32')
-
+#mixed_precision.set_global_policy('mixed_float16')
 
 #wandb.init(project='retrain', entity='holly-rafique')
 
@@ -131,7 +109,17 @@ def get_gpu_memory_usage():
     memory_usage = int(output[1]) if len(output) > 1 else 0
     return memory_usage
 
-
+def set_precision_policy():
+    major, minor = get_gpu_compute_capability()
+    if major is None:
+        print("No GPU found. Defaulting to float32.")
+        tf.keras.mixed_precision.set_global_policy('float32')
+    elif major >= 7:
+        print(f"GPU with compute capability {major}.{minor} detected. Setting mixed precision policy.")
+        tf.keras.mixed_precision.set_global_policy('mixed_float16')
+    else:
+        print(f"GPU with compute capability {major}.{minor} detected. Defaulting to float32.")
+        tf.keras.mixed_precision.set_global_policy('float32')
 
 def data_loader(path,config):
     
@@ -211,7 +199,6 @@ def main(args,config,name,save_path):
     
     # Start a run, tracking hyperparameters
     #wandb.init(project='retrain', entity='holly-rafique')
-
     #HR 13/06/2024
     set_precision_policy()
 
@@ -265,10 +252,6 @@ def main(args,config,name,save_path):
         #with tf.device('/cpu:0'):
         model=FUNCMODELS[args.model_name](**model_params)
         model=model.build()
-        for i,layer in enumerate(model.layers):
-            print("\nlayer",i)
-            for weight in layer.weights:
-                print(weight.name,weight) 
  
     #HOLLY - this is different to holly-old-branch
     # check when GV added
@@ -303,6 +286,8 @@ def main(args,config,name,save_path):
     #I think this is returning the final model rather than the best
     run_profiling= config['profile'] if 'profile' in config else False
     model, history = train.forward(run_profiling)
+ 
+    # model, history = train.forward()
     #save model, config and training curves
     model_save_path=os.path.join(save_path,'models')
     #save_experiment(model,config,history,name,model_save_path)

src/predict.py
@@ -25,8 +25,8 @@ from stitching import Canvas, stitch
 DEBUG = True
 
 def dice_coef(y_true,y_pred,idx=[0,2,3],smooth=1):
-        y_true=y_true.type(torch.float16) #float32)
-        y_pred=y_pred.type(torch.float16) #float32)
+        y_true=y_true.type(torch.float32)
+        y_pred=y_pred.type(torch.float32)
         intersection=torch.sum(y_true*y_pred,dim=idx)
         union=torch.sum(y_true,dim=idx)+torch.sum(y_pred,dim=idx)
         dice=torch.mean((2*intersection+smooth)/(union+smooth),dim=0)
@@ -99,7 +99,7 @@ class Predict():
             patch=np.expand_dims(patch,axis=0)
             logits=self.model(patch)
             #print("logits:",logits.shape)
-            prediction=tf.cast((logits>self.threshold), tf.float16) #tf.float32)
+            prediction=tf.cast((logits>self.threshold), tf.float32)
             #print("preds:",prediction.shape)
             stitch(c, prediction, y, x, y_dim, x_dim, self.tile_dim, self.step, margin)
             #plt.imshow(c.canvas[0,:,:,:])

src/predict_tor.py
@@ -40,7 +40,7 @@ class Predict():
             patch=image[y:y+self.step,x:x+self.step,:]
             patch=np.expand_dims(patch,axis=0)
             probs=self.model.predict(patch)
-            prediction=tf.cast((probs>self.threshold), tf.float32)
+            prediction=tf.cast((probs>self.threshold),tf.tf.float32)
             canvas[:,y:y+self.step,x:x+self.step,:]=prediction
         return canvas.astype(np.uint8)
 

src/utilities/augmentation.py
@@ -14,7 +14,6 @@ import tensorflow_addons as tfa
 import matplotlib.pyplot as plt
 import imgaug.augmenters as iaa
 import staintools
-from tiatoolbox.tools.stainaugment import StainAugmentor
 
 __author__ = 'Gregory Verghese'
 __email__ ='gregory.verghese@kcl.ac.uk'
@@ -49,8 +48,8 @@ def is_uint8_image(I):
     return True
 
 # Function to standardize brightness and apply stain augmentation
-#@tf.py_function #(Tout=tf.float16)
-def stain_augment_image(image,method='tia-toolbox'):
+#@tf.py_function(Tout=tf.float32)
+def stain_augment_image(image):
     stain_method='macenko'
     sig1 = np.random.uniform(0.55,0.85)
     sig2 = np.random.uniform(0.55,0.85)
@@ -58,35 +57,23 @@ def stain_augment_image(image,method='tia-toolbox'):
     image_np = image.numpy()  # Convert TensorFlow tensor to NumPy array
     #print("in stain_augment_image...image_np max:",np.max(image_np),image_np.shape) 
 
-    # Convert from float16 to uint8 for staintools processing
+    # Convert from float32 to uint8 for staintools processing
     image_np = image_np.astype(np.uint8)
     #print(image_np.shape,image_np.dtype)
 
-    if method=='staintools':
-        # Standardize brightness
-        image_standardized = staintools.LuminosityStandardizer.standardize(image_np)
+    # Standardize brightness
+    image_standardized = staintools.LuminosityStandardizer.standardize(image_np)
     
-        # Stain augment
-        augmentor = staintools.StainAugmentor(method=stain_method, sigma1=sig1, sigma2=sig2, augment_background=False)
-        augmentor.fit(image_standardized)
-        augmented_image = augmentor.pop()
+    # Stain augment
+    augmentor = staintools.StainAugmentor(method=stain_method, sigma1=sig1, sigma2=sig2, augment_background=False)
+    augmentor.fit(image_standardized)
+    augmented_image = augmentor.pop()
 
-    elif method=='tia-toolbox':
-        stain_augmentor = StainAugmentor(method=stain_method,sigma1=sig1, sigma2=sig2, augment_background=False)
-        augmented_image = stain_augmentor.apply(image)
-
-    else:
-        augmented_image = image
-
-    # Convert the augmented image back to float16
-    augmented_image_float16 = augmented_image.astype(np.float16)
+    # Convert the augmented image back to float32
+    augmented_image_float16 = augmented_image.astype(np.float32)
     
     
-    return tf.convert_to_tensor(augmented_image, dtype=tf.float16)  # Convert back to TensorFlow tensor
-
-
-
-
+    return tf.convert_to_tensor(augmented_image, dtype=tf.float32)  # Convert back to TensorFlow tensor
 
 
 
@@ -118,7 +105,7 @@ class Augment():
     '''
 
     def __init__(self, hueLimits, saturationLimits, contrastLimits, brightnessLimits,
-                 rotateProb=0.5, flipProb=0.5, colorProb=0.5,stain_method='macenko',aug_method='tia-toolbox'): 
+                 rotateProb=0.5, flipProb=0.5, colorProb=0.5,stain_method='macenko'): 
 
         self.hueLimits = hueLimits
         self.saturationLimits = saturationLimits
@@ -128,7 +115,6 @@ class Augment():
         self.flipProb = flipProb
         self.colorProb = colorProb
         self.stain_method = stain_method
-        self.aug_method = aug_method
     
 
     def getRotate90(self, x, y):
@@ -236,13 +222,13 @@ class Augment():
     def getStainAugment(self, x, y):
         #print("x type:",x.dtype,x.shape)
         
-        #augmented_image = tf.py_function(stain_augment_image, [x], tf.float16)
+        #augmented_image = tf.py_function(stain_augment_image, [x], tf.float32)
         #augmented_image = stain_augment_image(x)
 
         augmented_images = tf.map_fn(
-        lambda img: tf.py_function(stain_augment_image, [img], tf.float16),
+        lambda img: tf.py_function(stain_augment_image, [img], tf.float32),
             x,
-            dtype=tf.float16
+            dtype=tf.float32
         )
         augmented_images.set_shape(x.shape)  # Ensure the shape information is retained
         return augmented_images, y